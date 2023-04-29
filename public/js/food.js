2fcace683721a75c45cf523c838fc6f83a303591
Joey Hou <joeyhou0804@gmail.com>
2023-04-29 00:26:45 -0700

    add and close animation

--- public/js/food.js
+++ public/js/food.js
@@ -4,8 +4,12 @@ $(document).ready(function(){
   $("#add-button").click(function() {
     if ( $("#add-menu").first().is( ":hidden" ) ) {
       $("#add-menu").slideDown("slow");
+      $("#add-image").attr("src", "../images/food_item_close.png");
+      $("#add-text").text("Close");
     } else {
-      $("#add-menu").slideUP("slow");
+      $("#add-menu").slideUp("slow");
+      $("#add-image").attr("src", "../images/food_item_add.png");
+      $("#add-text").text("Add...");
     }
   });
 });
