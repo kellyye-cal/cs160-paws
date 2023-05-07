c624dfd79a3d1d49f0946fb57d6f9d11630fe7ef
Joey Hou <joeyhou0804@gmail.com>
2023-05-07 01:37:08 -0700

    finished profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,8 +1,12 @@
 let info_selection = 0;
+let record_selection = 0;
 
 $(document).ready(function(){
   $("#info-change").hide();
   $("#are-you-sure-1").hide();
+
+  $("#record-change").hide();
+  $("#are-you-sure-2").hide();
 
   $("#info-edit").click(function() {
     if ($("#info-change").first().is( ":hidden" ) ){
@@ -24,6 +28,7 @@ $(document).ready(function(){
        }
    });
 
+  // GENERAL INFO
   $("#confirm-info").click(function() {
     let index = info_selection;
     let id = "#info-" + index.toString();
@@ -39,6 +44,7 @@ $(document).ready(function(){
     }
   });
 
+  // ARE YOU SURE FOR GENERAL INFO
   $("#confirm-1").click(function() {
     let index = info_selection;
     let id = "#info-" + index.toString();
@@ -52,5 +58,56 @@ $(document).ready(function(){
   $("#cancel-1").click(function() {
     $("#are-you-sure-1").slideUp("slow");
   });
+
+  $("#record-edit").click(function() {
+    if ($("#record-change").first().is( ":hidden" ) ){
+      $("#record-change").slideDown("slow");
+    } else {
+      $("#record-change").slideUp("slow");
+    }
+  });
+
+  $("#inputGroupSelect02").change(function () {
+       let index = this.value;
+       record_selection = index;
+       let id = "#record-" + index;
+       let value = $(id).text().trim();
+       if (value != "- No data -") {
+        $("#record-change-val").val($(id).text().trim());
+       } else {
+        $("#record-change-val").val("");
+       }
+   });
+
+  // IMPORTANT RECORDS
+  $("#confirm-record").click(function() {
+    let index = record_selection;
+    let id = "#record-" + index.toString();
+    let value = $("#record-change-val").val().trim();
+    if (value != "") {
+      $(id).text(value);
+      $(id).attr('class', 'col-6 text-black');
+      $("#record-change").slideUp("slow");
+    } else {
+      if (id != "#record-0") {
+        $("#are-you-sure-2").slideDown("slow");
+      }
+    }
+  });
+
+    // ARE YOU SURE FOR IMPORTANT RECORDS
+    $("#confirm-2").click(function() {
+      let index = record_selection;
+      let id = "#record-" + index.toString();
+  
+      $(id).text("- No data -");
+      $(id).attr('class', 'col-6 text-gray');
+  
+      $("#record-change").slideUp("slow");
+      $("#are-you-sure-2").slideUp("slow");
+    });
+    $("#cancel-2").click(function() {
+      $("#are-you-sure-2").slideUp("slow");
+    });
 
 });
