a569a2424af7d7454004d20ae529deca66a38010
Joey Hou <joeyhou0804@gmail.com>
2023-05-06 22:39:18 -0700

    working navbar between health and food

---
+++ public/js/navbar.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(document.body).on('click', "#health-page", function() {
+    window.location.replace("../health/health.html");
+  });
+  $(document.body).on('click', "#food-page", function() {
+    window.location.replace("../food/main.html");
+  });
+});
