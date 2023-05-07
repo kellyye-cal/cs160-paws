535a014dd3a8f29af8c54c8b6546d6b2f2be51ed
Joey Hou <joeyhou0804@gmail.com>
2023-05-06 23:27:33 -0700

    new profile page

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  $(document.body).on('click', "#info-page", function() {
+    window.location.replace("../life/profile.html");
+  });
   $(document.body).on('click', "#health-page", function() {
     window.location.replace("../health/health.html");
   });
