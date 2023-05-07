0d182190d2d9a59a47f227994283b188a67105bb
Joey Hou <joeyhou0804@gmail.com>
2023-05-07 02:04:43 -0700

    redo moments form

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   $(document.body).on('click', "#info-page", function() {
-    window.location.replace("../life/profile.html");
+    window.location.replace("../profile/profile.html");
+  });
+  $(document.body).on('click', "#moments-page", function() {
+    window.location.replace("../moments/moments.html");
   });
   $(document.body).on('click', "#health-page", function() {
     window.location.replace("../health/health.html");
